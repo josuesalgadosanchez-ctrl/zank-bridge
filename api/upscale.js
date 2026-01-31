bdea288790ac8cd69f05fabbedc3a290e719a623
josuesalgadosanchez-ctrl <josuesalgadosanchez@gmail.com>
2026-01-31 03:33:24 -0600

    Update upscale.js

--- api/upscale.js
+++ api/upscale.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://zankstudios.com');
+    // Estas líneas permiten que tu Shopify se conecte sin errores de seguridad
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
